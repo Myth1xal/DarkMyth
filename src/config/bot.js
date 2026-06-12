fb9344bb3d7ed695463b8b420df1a05fa3f888d1
Myth1xal <notmyth1xal@gmail.com>
2026-06-12 15:47:01 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -59,6 +59,8 @@ export const botConfig = {
       { question: "What is your name?", required: true },
       { question: "How old are you?", required: true },
       { question: "Why do you want to join?", required: true },
+      { question: "List your usernames on roblox and discord", required: true },
+      { question: "Is there anything else you want use to know?", required: false },
     ],
 
     // Embed colors by application status.
